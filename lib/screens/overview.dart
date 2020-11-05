903f7440160c91aef944fec52c6789d2641b2b51
Ryan Palmer <ryanpnayr@gmail.com>
2020-11-04 20:49:32 -0500

    Getting overview tabs to scroll and work the way we want them to

--- lib/screens/overview.dart
+++ lib/screens/overview.dart
@@ -8,9 +8,12 @@ class Overview extends StatefulWidget {
   _OverviewState createState() => new _OverviewState();
 }
 
-class _OverviewState extends State<Overview> {
+class _OverviewState extends State<Overview>
+    with SingleTickerProviderStateMixin {
   AppState appState;
   ThemeData theme;
+  TabController controller;
+
   List<MilestoneTransition> milestones = [
     MilestoneTransition(
         fromMilestone: '', toMilestone: 'First Meeting', completed: true),
@@ -22,69 +25,89 @@ class _OverviewState extends State<Overview> {
         fromMilestone: 'Final Meeting',
         toMilestone: 'Final documents delivered'),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     theme = Theme.of(context);
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          backgroundColor: theme.secondaryHeaderColor,
-          appBar: AppBar(
-            shape: Border(bottom: BorderSide(color: Colors.white)),
-            automaticallyImplyLeading: false,
-            backgroundColor: theme.secondaryHeaderColor,
-            toolbarHeight: 150,
-            elevation: 0,
-            title: Container(
-              margin: EdgeInsets.only(left: 40),
-              child: Wrap(
-                crossAxisAlignment: WrapCrossAlignment.start,
-                alignment: WrapAlignment.start,
-                direction: Axis.vertical,
-                spacing: 10,
-                children: [
-                  Text(
-                    "Your Overview",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Container(
-                    width: 300,
-                    child: Text(
-                      "One place to track your progress",
-                      style: TextStyle(fontSize: 30),
-                      maxLines: 2,
+    return Scaffold(
+      backgroundColor: theme.secondaryHeaderColor,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              pinned: false,
+              shape: Border(bottom: BorderSide(color: Colors.white)),
+              automaticallyImplyLeading: false,
+              backgroundColor: theme.secondaryHeaderColor,
+              toolbarHeight: 100,
+              elevation: 0,
+              flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.parallax,
+                background: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Text(
+                        "Your Overview",
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        textAlign: TextAlign.left,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            bottom: TabBar(
-              indicator: UnderlineTabIndicator(
-                borderSide:
-                    BorderSide(width: 4.0, color: theme.bottomAppBarColor),
-                insets: EdgeInsets.symmetric(horizontal: 60.0),
-              ),
-              tabs: [usersMilestoneTitle, splitLegalCurrentMilestoneTitle],
-            ),
-          ),
-          body: Column(
-            children: [
-              Flexible(
-                fit: FlexFit.loose,
-                child: TabBarView(
-                  children: [
-                    userMilestones,
-                    splitLegalCurrentMilestones,
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "One place to track your progress",
+                        style: TextStyle(fontSize: 30.0, color: Colors.white),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Container(
-                width: double.infinity,
-                color: theme.primaryColor,
-                child: Text("Hello"),
+              expandedHeight: 200.0,
+              bottom: TabBar(
+                indicator: UnderlineTabIndicator(
+                  borderSide:
+                      BorderSide(width: 4.0, color: theme.bottomAppBarColor),
+                  insets: EdgeInsets.symmetric(horizontal: 60.0),
+                ),
+                tabs: [usersMilestoneTitle, splitLegalCurrentMilestoneTitle],
+                controller: controller,
               ),
-            ],
-          )),
+            )
+          ];
+        },
+        body: TabBarView(
+          controller: controller,
+          children: [
+            wrapOverviewTab(userMilestones),
+            wrapOverviewTab(splitLegalCurrentMilestones),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget wrapOverviewTab(Widget mainView) {
+    return Column(
+      children: [
+        mainView,
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(top: 20),
+            width: double.infinity,
+            color: theme.primaryColor,
+            child: Text("Hello"),
+          ),
+        ),
+      ],
     );
   }
 
